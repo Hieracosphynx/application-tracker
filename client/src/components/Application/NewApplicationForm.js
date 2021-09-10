baba3cdf8ffed802aa7507600fe1c85cf9576df0
Hieracosphynx <cortezmiguel05@gmail.com>
2021-09-10 13:05:15 -0700

    Change border textfield

--- client/src/components/Application/NewApplicationForm.js
+++ client/src/components/Application/NewApplicationForm.js
@@ -2,10 +2,27 @@ import { Fragment } from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 import TableCell from '@material-ui/core/TableCell';
+import TextField from '@material-ui/core/TextField';
 
-const applicationFormStyle = makeStyles(() => ({
+const applicationFormStyle = makeStyles((theme) => ({
+  textFields: {
+    margin: theme.spacing(1),
+    '& .MuiOutlinedInput-notchedOutline': {
+      borderColor: 'white',
+    },
+    '& .MuiOutlinedInput-input': {
+      color: 'white',
+    },
+  },
+  input: {
+    color: 'white',
+  },
+  label: {
+    borderColor: 'white',
+  },
   tableCells: {
     color: 'white',
+    borderColor: 'white',
   },
 }));
 
@@ -17,9 +34,39 @@ const NewApplicationForm = (props) => {
 
   return (
     <Fragment>
-      <TableCell className={classes.tableCells}></TableCell>
-      <TableCell className={classes.tableCells}></TableCell>
-      <TableCell className={classes.tableCells}></TableCell>
+      <TableCell className={classes.tableCells}>
+        <TextField
+          className={classes.textFields}
+          color='primary'
+          variant='outlined'
+          size='small'
+          InputProps={{
+            className: classes.input,
+          }}
+        />
+      </TableCell>
+      <TableCell align='center'>
+        <TextField
+          className={classes.textFields}
+          color='primary'
+          variant='outlined'
+          size='small'
+          InputProps={{
+            className: classes.input,
+          }}
+        />
+      </TableCell>
+      <TableCell align='center'>
+        <TextField
+          className={classes.textFields}
+          color='primary'
+          variant='outlined'
+          size='small'
+          InputProps={{
+            className: classes.input,
+          }}
+        />
+      </TableCell>
     </Fragment>
   );
 };
